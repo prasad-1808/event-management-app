95211e444bb4baac6abc07af2e4ab0a72c5604b2
prasad-1808 <prasadvenki60@gmail.com>
2025-08-24 02:34:38 +0530

    Application dockerized

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myappdb',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'postgres',
+        'PORT': '5432',
     }
 }
 
